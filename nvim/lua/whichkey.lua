d20c2dd4b94fdfc238c809a88aafc98b342a2f60
Gareth Hughes <hughesrgareth@proton.me>
2023-11-18 20:25:58 +1100

    LSP setup

--- nvim/lua/whichkey.lua
+++ nvim/lua/whichkey.lua
@@ -9,7 +9,7 @@ local setup = {
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         spelling = {
             enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-            suggestions = 20, -- how many suggestions should be shown in the list?
+            suggestions = 20 -- how many suggestions should be shown in the list?
         },
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
@@ -20,8 +20,8 @@ local setup = {
             windows = true, -- default bindings on <c-w>
             nav = true, -- misc bindings to work with windows
             z = true, -- bindings for folds, spelling and others prefixed with z
-            g = true, -- bindings for prefixed with g
-        },
+            g = true -- bindings for prefixed with g
+        }
     },
     -- add operators that will trigger motion and text object completion
     -- to enable all native operators, set the preset / operators plugin above
@@ -36,27 +36,33 @@ local setup = {
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
-        group = "+", -- symbol prepended to a group
+        group = "+" -- symbol prepended to a group
     },
     popup_mappings = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>" -- binding to scroll up inside the popup
     },
     window = {
         border = "rounded", -- none, single, double, shadow
         position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0,
+        margin = {1, 0, 1, 0}, -- extra window margin [top, right, bottom, left]
+        padding = {2, 2, 2, 2}, -- extra window padding [top, right, bottom, left]
+        winblend = 0
     },
     layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
+        height = {
+            min = 4,
+            max = 25
+        }, -- min and max height of the columns
+        width = {
+            min = 20,
+            max = 50
+        }, -- min and max width of the columns
         spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
+        align = "left" -- align columns left, center or right
     },
     ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+    hidden = {"<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
     show_help = true, -- show help message on the command line when the popup is visible
     triggers = "auto", -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specify a list manually
@@ -64,9 +70,9 @@ local setup = {
         -- list of mode / prefixes that should never be hooked by WhichKey
         -- this is mostly relevant for key maps that start with a native binding
         -- most people should not need to change this
-        i = { "j", "k" },
-        v = { "j", "k" },
-    },
+        i = {"j", "k"},
+        v = {"j", "k"}
+    }
 }
 
 local opts = {
@@ -75,11 +81,11 @@ local opts = {
     buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
     silent = true, -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+    nowait = true -- use `nowait` when creating keymaps
 }
 
 local mappings = {
-    ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
+    ["e"] = {"<cmd>NvimTreeToggle<cr>", "Explorer"}
 }
 
 which_key.register(mappings, opts)
